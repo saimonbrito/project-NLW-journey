3960d678f1150782f29a30df51089af0dfe07573
mauricio fernandes <britomauricio89@gmail.com>
2024-07-14 09:22:18 -0300

    finalizando o template

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,11 +1,18 @@
-import { ArrowRight , Calendar,MapPin,UserRoundPlus, Settings2,AtSign,Plus} from "lucide-react";
-import { useState } from "react";
+import { ArrowRight , Calendar,MapPin,UserRoundPlus, AtSign,Plus,X,Settings2} from "lucide-react";
+import { FormEvent, useState } from "react";
+
 
 
 
 export function Main(){
     const [ box,setBox] = useState(false)
     const [ boxModal,setBoxModal] = useState(false)
+    const [adcionarEmailmodal, setAdcionarEmailmodal] = useState([
+         'brito@brito.com' 
+         ,'saikas@amores.com',
+         'saikas@amores.com'
+
+    ])
 
     function funBox(e:any){
         e.preventDefault()
@@ -16,11 +23,43 @@ export function Main(){
         e.preventDefault()
         setBox(false)
     }
+
     function funBoxModal(e:any){
         e.preventDefault()
         setBoxModal(true)
     }
+    function fexarFunBoxModal(e:any){
+        e.preventDefault()
+        setBoxModal(false)
+    }
+    
+    function emailSubmit(e:FormEvent<HTMLFormElement>){
+        e.preventDefault()
+        const data =new FormData(e.currentTarget)
+        const email = data.get('email')?.toString()
 
+
+       if(!email){
+        return
+       }
+       if (adcionarEmailmodal.includes(email)){
+        return
+       }
+
+        setAdcionarEmailmodal([
+            ...adcionarEmailmodal,
+            email
+        ])
+
+        e.currentTarget.reset()
+
+
+    }
+
+    function removeEmailSubmit(emailRemove:string){
+        const newEmailList = adcionarEmailmodal.filter(email => email !== emailRemove)
+      setAdcionarEmailmodal(newEmailList)
+    }
 
 
 
@@ -28,13 +67,13 @@ export function Main(){
         <div>
             <form className="flex flex-col gap-3">
                 <div className=" max-w-3xl w-full bg-zinc-900 text-zinc-400 p-4 flex gap-4 rounded-lg">
-                  <div className=" flex items-center gap-2 flex-1">
+                  <div className="  flex items-center gap-2 flex-1">
                   <MapPin className="size-5 text-zinc-400"/> 
-                  <input disabled={box} className="bg-transparent w-80 text-zinc-400"  placeholder="Para onde você vai?" type="Para onde você vai?"  />
+                  <input disabled={box} className="outline-none bg-transparent w-80 text-zinc-400"  placeholder="Para onde você vai?" type="Para onde você vai?"  />
                   </div>
                      <div className=" flex items-center gap-2">
                      <Calendar className="size-5 text-zinc-400"/>
-                     <input disabled={box} className="bg-transparent w-20 "  placeholder=" Quando?" type="Quando?" />
+                     <input disabled={box} className=" outline-none bg-transparent w-20 "  placeholder=" Quando?" type="Quando?" />
                      </div>
                     <div className="">
                    {box ?(
@@ -65,21 +104,52 @@ export function Main(){
             {boxModal &&
             (  
                 <div className="fixed inset-0 bg-black/60 flex justify-center items-center">
-                   <div className="w-[640px] px-6 py-5 rounded-xl bg-zinc-900">
-                     <div>
-                     <h1>selecionar convidados</h1>
-                     <p>Os convidados irão receber e-mails para confirmar a participação na viagem.</p> 
-                     </div>
-                     
+                    <div className="w-[640px] px-6 py-5 rounded-xl bg-zinc-900">
 
-                     <div className="flex  justify-between items-center px-6 py-5  bg-zinc-950 rounded-md">
-                        <AtSign className="size-5 text-zinc-400"/>
-                        <p className="text-zinc-400">Digite o e-mail do convidado</p>
-                        <div>
-                            <button  className="bg-lime-300 text-lime-950 px-3 hover:bg-lime-400 rounded-md flex  items-center gap-1 font-medium">convidado <Plus className="size-5 text-zinc-400"/></button>
+                        <div className="flex justify-between">
+                            <h1 className="text-white">Selecionar convidados</h1>
+                            <X className="text-zinc-400 w-5 " onClick={fexarFunBoxModal}/>
                         </div>
-                     </div>
+                            <p className="text-zinc-400">Os convidados irão receber e-mails para confirmar a participação na viagem.</p> 
+
+                        <div className="py-5 px-6 flex flex-wrap gap-2" > 
+
+                           {adcionarEmailmodal.map(email => {
+                            return( 
+
+                                <div key={email} className=" rounded-md bg-zinc-800  flex items-center ">
+                               <span className="px-2 py-1 text-zinc-300">{email}</span>
+                               <button type="button" onClick={()=> removeEmailSubmit(email)}>
+                               <X className=" text-zinc-400 w-4" />
+                               </button>
+                               </div>
+                            )
+                           })}
+
+                        </div>
+
+                            <form onSubmit={emailSubmit} className="flex  justify-between items-center px-6 py-5  bg-zinc-950 rounded-md">
+                                <div className="flex gap-3 rounded-lg">
+                                    <AtSign className="size-5 text-zinc-400"/>
+                                    <input 
+                                    type='email'
+                                     name="email"
+                                     placeholder="  Digite o e-mail do convidado" 
+                                    className=" outline-none bg-transparent w-full size-5 px-4 py-2 bg-zinc-950 placeholder:text-zinc-400 text-zinc-400 flex-1" type="Digite o e-mail do convidado" />
+                                </div>
+
+                                <div>
+                                <button type="submit" className="bg-lime-300 text-lime-950 px-5 py-2 hover:bg-lime-400 rounded-lg flex  items-center gap-1 font-medium">
+                                    convidado 
+                                    <Plus className="size-5 text-zinc-950"/>
+                                </button>
+                                </div>
+
+                            </form>
+                       
+                        
                     </div>
+                    
                 </div>
 
             )}
---
+++ src/components/modal.tsx
@@ -0,0 +1,6 @@
+import {} from "lucide-react";
+export function Modal(){
+    return(
+        <h1>dd</h1>
+    )
+}
